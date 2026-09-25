1ba0cb0e8cf94d41f436fe404ebcb12533a6efdd
sameer786 <sameeralishaik7867@gmail.com>
2026-09-25 16:01:59 +0530

    Terraform Started

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,7 +1,18 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_all"
-  description = "Allow all traffic"
-      ingress {
+resource "aws_instance" "roboshop" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+  #vpc_security_group_ids = local.sg_id
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+    name        = "allow_all_change"
+    description = "allow all traffic"
+
+    ingress {
         from_port        = 0
         to_port          = 0
         protocol         = "-1"
@@ -15,7 +26,12 @@ resource "aws_security_group" "allow_tls" {
         cidr_blocks      = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
-  tags = {
-      Name = "allow-all"
-  }
-  }
+
+    lifecycle {
+      create_before_destroy = true
+    }
+
+    tags = {
+        Name = "allow-all"
+    }
+}
